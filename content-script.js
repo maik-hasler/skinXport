4c72c480890ac99d263de56f5d26ceb6731afb95
Maik Hasler <maikhslr@gmail.com>
2023-12-06 13:07:16 +0100

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -6,38 +6,34 @@ chrome.runtime.sendMessage({ type: 'fetchData' }, response => {
 const observer = new MutationObserver(appendQuickSummaries); 
 observer.observe(document.body, {subtree: true, childList: true});
 
-const customTag = `tag-${Date.now()}`
+const customTag = `tag-${Date.now()}`;
 
 function appendQuickSummaries() {
   const sales = document.getElementsByClassName('CatalogPage-item');
   for (let n = 0; n < sales.length; n++) {
-     const sale = sales[n];
-     if (!sale.hasAttribute(customTag)) { 
-       addQuickSummary(sale);
-       sale.setAttribute(customTag, true);
-     }
+    const sale = sales[n];
+    if (!sale.hasAttribute(customTag)) { 
+      const marketHashName = sale.querySelector('.ItemPreview-itemImage > img').getAttribute('alt');
+      // fetch data here
+      // pass data to addQuickSummaryForSale
+      var recentSaleStatistics = undefined;
+      addQuickSummaryForSale(sale, recentSaleStatistics);
+      sale.setAttribute(customTag, true);
+    }
   }
 }
 
-function addQuickSummary(sale) {
-  const marketHashName = sale.querySelector('.ItemPreview-itemImage > img').getAttribute('alt');
-
-  // prev:
+function addQuickSummaryForSale(sale, recentSaleStatistics) {
   const quickSummaryContainer = document.createElement('div');
-
   const horizontalLine = document.createElement('hr');
   horizontalLine.className = 'skinXport-hr';
-  quickSummaryContainer.append(horizontalLine);
-
   const title = document.createElement('span');
   title.innerText = 'Quick summary:';
   title.className = 'skinXport-heading';
-  quickSummaryContainer.append(title);
-
   const lastSevenDays = createStatistics(4, 5.67, 4.5, 69);
-
+  quickSummaryContainer.append(horizontalLine);
+  quickSummaryContainer.append(title);
   quickSummaryContainer.append(lastSevenDays);
-
   const target = sale.querySelector('.ItemPreview-link')
   target.append(quickSummaryContainer);
 }
@@ -61,7 +57,7 @@ function createColoredCircle(color) {
   return circle;
 }
 
-function createStatistics(min, max, avg, vol) {
+function createStatistics(recentSaleStatistics) {
   const lastXDaysContainer = document.createElement('div');
 
   // Title:
@@ -71,22 +67,22 @@ function createStatistics(min, max, avg, vol) {
 
   // Min:
   const minContainer = document.createElement('div');
-  minContainer.innerText = `Minimum: ${min}`;
+  minContainer.innerText = `Minimum: ${recentSaleStatistics['last_7_days']['min']}`;
   minContainer.className = 'skinXport-text';
 
   // Max:
   const maxContainer = document.createElement('div');
-  maxContainer.innerText = `Maximum: ${max}`;
+  maxContainer.innerText = `Maximum: ${recentSaleStatistics['last_7_days']['max']}`;
   maxContainer.className = 'skinXport-text';
 
   // Avg:
   const avgContainer = document.createElement('div');
-  avgContainer.innerText = `Average: ${avg}`;
+  avgContainer.innerText = `Average: ${recentSaleStatistics['last_7_days']['avg']}`;
   avgContainer.className = 'skinXport-text';
 
   // Vol:
   const volContainer = document.createElement('div');
-  volContainer.innerText = `Volume: ${vol}`;
+  volContainer.innerText = `Volume: ${recentSaleStatistics['last_7_days']['vol']}`;
   volContainer.className = 'skinXport-text';
 
   lastXDaysContainer.append(lastXDaysTitle);
