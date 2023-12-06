e0a797e357b850e5a2a85c15b6ff551178c04df4
maik-hasler <maikhslr@gmail.com>
2023-12-06 15:56:32 +0100

    various changes

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
 import { openDatabase, addData } from './database.js';
 
 chrome.alarms.create('fetchDataAlarm', {
-  periodInMinutes: 5,
+  periodInMinutes: 1,
   when: Date.now()
 });
-
 
 chrome.alarms.onAlarm.addListener(async alarm => {
   if (alarm.name === 'fetchDataAlarm') {
@@ -14,12 +13,13 @@ chrome.alarms.onAlarm.addListener(async alarm => {
 
       const transformedData = {};
 
-      data.forEach(item => {
+      for (let i = 0; i < data.length; i++) {
+        const item = data[i];
         const marketHashName = item["market_hash_name"];
         transformedData[marketHashName] = {
           last_7_days: item["last_7_days"]
         };
-      });
+      }
 
       const db = await openDatabase();
 
--- content.js
+++ content.js
@@ -3,90 +3,116 @@ observer.observe(document.body, {subtree: true, childList: true});
 
 const customTag = `tag-${Date.now()}`;
 
-async function appendQuickSummaries() {
+const request = indexedDB.open('SkinportSalesDB', 1);
+const db = request.result;
+
+function appendQuickSummaries() {
   const sales = document.getElementsByClassName('CatalogPage-item');
-  for (const sale of sales) {
+  for (let n = 0; n < sales.length; n++) {
+    const sale = sales[n];
     if (!sale.hasAttribute(customTag)) {
       const marketHashName = sale.querySelector('.ItemPreview-itemImage > img').getAttribute('alt');
-      const db = await openDatabase();
-      try {
-        var recentSaleStatistics = await getData(db, marketHashName);
-        addQuickSummaryForSale(sale, recentSaleStatistics);
-        sale.setAttribute(customTag, true);
-      } catch (error) {
-        console.error("Error getting data:", error);
-      }
+      const recentSaleStatistics = getData(db, marketHashName);
+      // addQuickSummaryForSale(sale, recentSaleStatistics);
+      // sale.setAttribute(customTag, true);
+      console.log(recentSaleStatistics);
+      sale.setAttribute(customTag, true);
     }
   }
 }
 
-function addQuickSummaryForSale(sale, recentSaleStatistics) {
-  const quickSummaryContainer = document.createElement('div');
-  const horizontalLine = document.createElement('hr');
-  horizontalLine.className = 'skinXport-hr';
-  const title = document.createElement('span');
-  title.innerText = 'Quick summary:';
-  title.className = 'skinXport-heading';
-  const lastSevenDays = createStatistics(recentSaleStatistics);
-  quickSummaryContainer.append(horizontalLine);
-  quickSummaryContainer.append(title);
-  quickSummaryContainer.append(lastSevenDays);
-  const target = sale.querySelector('.ItemPreview-link')
-  target.append(quickSummaryContainer);
+function getData(db, skinName) {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction([objectStoreName], 'readonly');
+    const objectStore = transaction.objectStore(objectStoreName);
+
+    const request = objectStore.get(skinName);
+
+    request.onsuccess = function (event) {
+      const result = event.target.result;
+      if (result) {
+        resolve(result);
+      } else {
+        reject('Data not found');
+      }
+    };
+
+    request.onerror = function (event) {
+      reject('Error getting data');
+    };
+  });
 }
 
-function createColoredCircle(color) {
-  // Validate the color
-  if (color !== 'red' && color !== 'green' && color !== 'orange') {
-    console.error('Invalid color. Please use "red", "green", or "orange".');
-    return;
-  }
+// Perform operations on the database here
 
-  // Create a new div element
-  var circle = document.createElement('div');
 
-  // Apply the class name for styling
-  circle.className = 'quick-flip-evaluation';
+// function addQuickSummaryForSale(sale, recentSaleStatistics) {
+//   const quickSummaryContainer = document.createElement('div');
+//   const horizontalLine = document.createElement('hr');
+//   horizontalLine.className = 'skinXport-hr';
+//   const title = document.createElement('span');
+//   title.innerText = 'Quick summary:';
+//   title.className = 'skinXport-heading';
+//   const lastSevenDays = createStatistics(recentSaleStatistics);
+//   quickSummaryContainer.append(horizontalLine);
+//   quickSummaryContainer.append(title);
+//   quickSummaryContainer.append(lastSevenDays);
+//   const target = sale.querySelector('.ItemPreview-link')
+//   target.append(quickSummaryContainer);
+// }
 
-  // Set the background color
-  circle.style.backgroundColor = color;
+// function createColoredCircle(color) {
+//   // Validate the color
+//   if (color !== 'red' && color !== 'green' && color !== 'orange') {
+//     console.error('Invalid color. Please use "red", "green", or "orange".');
+//     return;
+//   }
 
-  return circle;
-}
+//   // Create a new div element
+//   var circle = document.createElement('div');
 
-function createStatistics(recentSaleStatistics) {
-  const lastXDaysContainer = document.createElement('div');
+//   // Apply the class name for styling
+//   circle.className = 'quick-flip-evaluation';
 
-  // Title:
-  const lastXDaysTitle = document.createElement('div');
-  lastXDaysTitle.innerText = `Last 7 days:`;
-  lastXDaysTitle.className = 'skinXport-minor-heading';
+//   // Set the background color
+//   circle.style.backgroundColor = color;
 
-  // Min:
-  const minContainer = document.createElement('div');
-  minContainer.innerText = `Minimum: ${recentSaleStatistics['last_7_days']['min']}`;
-  minContainer.className = 'skinXport-text';
+//   return circle;
+// }
 
-  // Max:
-  const maxContainer = document.createElement('div');
-  maxContainer.innerText = `Maximum: ${recentSaleStatistics['last_7_days']['max']}`;
-  maxContainer.className = 'skinXport-text';
+// function createStatistics(recentSaleStatistics) {
+//   const lastXDaysContainer = document.createElement('div');
 
-  // Avg:
-  const avgContainer = document.createElement('div');
-  avgContainer.innerText = `Average: ${recentSaleStatistics['last_7_days']['avg']}`;
-  avgContainer.className = 'skinXport-text';
+//   // Title:
+//   const lastXDaysTitle = document.createElement('div');
+//   lastXDaysTitle.innerText = `Last 7 days:`;
+//   lastXDaysTitle.className = 'skinXport-minor-heading';
 
-  // Vol:
-  const volContainer = document.createElement('div');
-  volContainer.innerText = `Volume: ${recentSaleStatistics['last_7_days']['vol']}`;
-  volContainer.className = 'skinXport-text';
+//   // Min:
+//   const minContainer = document.createElement('div');
+//   minContainer.innerText = `Minimum: ${recentSaleStatistics['last_7_days']['min']}`;
+//   minContainer.className = 'skinXport-text';
 
-  lastXDaysContainer.append(lastXDaysTitle);
-  lastXDaysContainer.append(minContainer);
-  lastXDaysContainer.append(maxContainer);
-  lastXDaysContainer.append(avgContainer);
-  lastXDaysContainer.append(volContainer);
+//   // Max:
+//   const maxContainer = document.createElement('div');
+//   maxContainer.innerText = `Maximum: ${recentSaleStatistics['last_7_days']['max']}`;
+//   maxContainer.className = 'skinXport-text';
 
-  return lastXDaysContainer;
-}
+//   // Avg:
+//   const avgContainer = document.createElement('div');
+//   avgContainer.innerText = `Average: ${recentSaleStatistics['last_7_days']['avg']}`;
+//   avgContainer.className = 'skinXport-text';
+
+//   // Vol:
+//   const volContainer = document.createElement('div');
+//   volContainer.innerText = `Volume: ${recentSaleStatistics['last_7_days']['vol']}`;
+//   volContainer.className = 'skinXport-text';
+
+//   lastXDaysContainer.append(lastXDaysTitle);
+//   lastXDaysContainer.append(minContainer);
+//   lastXDaysContainer.append(maxContainer);
+//   lastXDaysContainer.append(avgContainer);
+//   lastXDaysContainer.append(volContainer);
+
+//   return lastXDaysContainer;
+// }
